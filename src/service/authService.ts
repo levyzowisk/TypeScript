74e18a3a935075611a90b9230c8def4ff7523631
Levy <85988919231levy@gmail.com>
2024-11-06 23:20:19 -0300

    Introduzindo o jsonwebtoken

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,6 +1,9 @@
 import DbConnection from "../connection/DbConnection";
+import dotenv from 'dotenv'
+import jwt from "jsonwebtoken"
 
+dotenv.config()
 export const loginUser = (password: string  , email: string): string => {
 
-    
+    jwt.sign()
 } 
